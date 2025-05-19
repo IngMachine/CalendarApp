3ceeb110afc8dee2eedc9a16c57b6526c7faae3f
Fredy Rivera <fredyjesus2013@gmail.com>
2025-05-19 13:22:44 -0500

    fix: correct path syntax for serving index.html in catch-all route

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/api/auth', require('./routes/auth') );
 app.use('/api/events', require('./routes/events') );
 
 app.get('/{*any}', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen( process.env.PORT , () => {
