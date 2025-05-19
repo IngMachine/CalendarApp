de61c36e55da0512564957b3932a88fc598e1d56
Fredy Rivera <fredyjesus2013@gmail.com>
2025-05-19 13:20:42 -0500

    fix: update catch-all route to correctly serve index.html for unmatched requests

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.use( express.json() );
 app.use('/api/auth', require('./routes/auth') );
 app.use('/api/events', require('./routes/events') );
 
-app.get('*', (req, res) => {
-    res.sendFile( __dirname + '/public/index.html');
-})
+app.get('/{*any}', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen( process.env.PORT , () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
